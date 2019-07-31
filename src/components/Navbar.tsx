937a9f26d01f9c7115a0a2572c6001b88633eac4
Sean Gay <sean@sixtydigits.com>
2019-07-31 12:17:34 +1000

    Completed #5 - accessing context values via provider method.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,22 +6,23 @@ type Props = {};
 type State = {}
 
 export class Navbar extends React.Component<Props, State> {
-  // noinspection JSUnusedGlobalSymbols
-  static contextType = ThemeContext;
-
   render() {
-    console.log("CONTEXT", this.context);
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{background: theme.ui, color: theme.syntax}}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context: any) => {
+        const {isLightTheme, light, dark} = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <nav style={{background: theme.ui, color: theme.syntax}}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}
+      </ThemeContext.Consumer>
     );
   };
 }
