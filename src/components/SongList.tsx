b16663a3b6f3f3b192a7393ddb8de2d5d1ea49a2
Sean Gay <sean@sixtydigits.com>
2019-07-31 12:49:56 +1000

    Completed #9 - Intro to hooks.
    - The tutorial series creates a completely new application to demo this stuff.  I have not.  I figure I'll be able to update content appropriately to demo the concepts required here.

---
+++ src/components/SongList.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export function SongList() {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>This Wild Darkness</li>
+        <li>Memory Gospel</li>
+      </ul>
+    </div>
+  );
+}
